1712485a1495a6ae6a96cb5c4d9fb73e6812e221
Mohd Hammad Khan <hammadkhan@Mohds-MacBook-Pro.local>
2023-01-11 01:21:36 -0400

    made mongo connection

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv")
+const mongoose = require("mongoose");
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 dotenv.config();
+
+mongoose.connect("process.env.mongo_connection", {useNewUrlParser : true}, ()=>{
+    console.log("Database connected");
+})
 
 app.get("/", (_req,res)=>{
     res.send("Welcome");

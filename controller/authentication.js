0f1a76b43f708a396567e8dd468158041f60687d
Mohd Hammad Khan <hammadkhn30@gmail.com>
2023-01-16 20:27:46 -0400

    removed md5 for more secure bcrypt

--- controller/authentication.js
+++ controller/authentication.js
@@ -1,40 +1,48 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const md5 = require("md5")
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user.js");
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.use(bodyparser.json());
 
 router
 .post("/signup", (req,res)=>{
-    const NewUser = new User({
-        name : req.body.name,
-        email : req.body.email,
-        password : md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const NewUser = new User({
+            name : req.body.name,
+            email : req.body.email,
+            password : hash
+        });
+        NewUser.save((err)=>{
+            if (err) {
+                res.status(500).json({message: "There was an error creating the user", error: err.message})
+            }else{
+                res.status(200).json({message : "Info added"})
+            }
+        })
     });
-    NewUser.save((err)=>{
-        if (err) {
-            res.status(500).json({message: "There was an error creating the user", error: err.message})
-        }else{
-            res.status(200).json({message : "Info added"})
-        }
-    })
 })
 router
 .post("/login", (req,res)=> {
     const username = req.body.email;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email : username}, (err, foundUser)=>{
         if (err) {
-            res.json({message : "user not found"})
+            res.status(404).json({message : "user not found"})
         }else{
             if (foundUser) {
-                if (foundUser.password == password) {
-                    res.json({message : "user found"})
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result == true) {
+                        res.status(401).json({message : "user found"});    
+                    }else{
+                        res.status(404).json({message : "wrong password"});
+                    }
+                });
             }
         }
     })
